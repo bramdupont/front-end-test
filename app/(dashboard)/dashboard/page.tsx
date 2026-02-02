8390839c95f32ae8b8cd1f2058f5e08915c7efa0
bramdupont <bram@byteklaar.be>
2026-02-02 13:25:53 +0100

    Feat: Default page for dashboard

---
+++ app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default async function Dashboard() {
+    return (
+        <div className="container mx-auto flex flex-col items-center px-4 sm:px-6 lg:px-8 text-white">
+            <h1 className="text-2xl font-bold">Dashboard</h1>
+            <p>Coming soon</p>
+        </div>
+    )
+}
